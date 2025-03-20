1724c68140947409535b4a6a6d89044dfa236012
mingeun23202 <geun3202@daum.net>
2025-03-20 21:03:32 +0900

    first commit

---
+++ week.3/p147-2-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int reverse(int);
+
+int main() {
+    int number;
+    
+
+    cout << "숫자를 입력하세요: ";
+    cin >> number;
+
+    cout << "역순 숫자: " << reverse(number) << endl;
+
+    return 0;
+}
+
+int reverse(int num)  {
+    int reversedNum = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num /= 10;
+    }
+
+    return reversedNum;
+}
---
+++ week.3/p1472-5 .cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Reverse.h"
+
+using namespace std;
+
+int reverse(int num)  {
+    int reversedNum = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 +digit;
+        num /=10;
+    }
+
+    return reversedNum;
+}
+
+int main()  {
+
+    cout << "숫자를 입력하세요: ";
+    cint >> number;
+    cout << "역순 숫자: " << reverse(number) << endl;
+
+    return 0;
+}
